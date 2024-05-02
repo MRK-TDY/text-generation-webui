b1a9c39d18cab0f4114939aca4ad0d1d9bb947b3
sapountzis <sapountzis.andreas@gmail.com>
2024-05-02 10:12:31 +0000

    clean similarity function

--- extensions/intent_detection/script.py
+++ extensions/intent_detection/script.py
@@ -16,13 +16,18 @@ def setup():
 
 
 async def intent_similarity(query: str, intent_samples: list[str], threshold: float = 0.8) -> float:
-    docs = [query] + intent_samples
     logger.info(f'Query: {query}')
     logger.info(f'Intent samples: {intent_samples}')
+    similarities = await calculate_similarity(query, intent_samples)
+    return max(similarities)
+
+
+async def calculate_similarity(query: str, texts: list[str], threshold: float = 0.8) -> float:
+    docs = [query] + texts
     async with httpx.AsyncClient() as client:
         response = await client.post(params["endpoint"], json=docs)
         embeddings = response.json()
     embeddings = torch.tensor(embeddings)
     similarities = cos_sim(embeddings[0], embeddings[1:]).flatten()
     logger.info(f'Similarities: {similarities}')
-    return max(similarities)
+    return similarities
